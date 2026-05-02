41bffa7e42d573999fb27c66ebcd63facf7bc5f3
Gangatharan2108 <gangatharan3149@gmail.com>
2026-05-02 18:31:02 +0530

    update backend api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://product-crud-production-2544.up.railway.app/api",
   headers: { "Content-Type": "application/json" },
 });
 
